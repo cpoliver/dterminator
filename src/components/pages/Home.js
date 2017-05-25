0ff215c0b514c36dee64568712426fb38fc1843b
Charles P. Oliver <cpoliver@gmail.com>
2017-05-25 22:35:13 +0200

    Add react-native-elements. Add basic home screen items

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, SocialIcon } from 'react-native-elements';
 
 class Home extends Component {
   render() {
     return (
       <View style={styles.view}>
-        <Text>Home</Text>
+        <Text style={styles.titleStyle}>dterminator</Text>
+        <Text>an app for the indecisive</Text>
+        <Text>made with love by @cpoliver</Text>
+        <View style={styles.iconContainer}>
+          <SocialIcon type="github" iconSize={styles.iconStyle.iconSize} />
+          <SocialIcon type="linkedin" iconSize={styles.iconStyle.iconSize} />
+          <SocialIcon type="twitter" iconSize={styles.iconStyle.iconSize} />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  iconContainer: {
+    flexDirection: 'row'
+  },
+  iconStyle: {
+    fontSize: 24
+  },
+  titleStyle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
   view: {
     flex: 1,
     justifyContent: 'center',
