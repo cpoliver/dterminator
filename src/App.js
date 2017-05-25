0d61dfb728ca130ebd8e3cffce2abc8b6a06de93
Charles P. Oliver <cpoliver@gmail.com>
2017-05-25 13:28:58 +0200

    Add basic swipe navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 class App extends Component {
   render() {
     return (
-      <View style={{ justifyContent: 'center' }}>
-        <Text>Hello, World</Text>
-      </View>
+      <Swiper loop={false} showsPagination={false} index={1}>
+        <View style={styles.view}>
+        <Text>Left</Text>
+        </View>
+        <Swiper loop={false} showsPagination={false} index={1} horizontal={false}>
+          <View style={styles.view}>
+            <Text>Top</Text>
+          </View>
+          <View style={styles.view}>
+            <Text>Home</Text>
+          </View>
+          <View style={styles.view}>
+            <Text>Bottom</Text>
+          </View>
+        </Swiper>
+        <View style={styles.view}>
+          <Text>Right</Text>
+        </View>
+      </Swiper>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default App;
