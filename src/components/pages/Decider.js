c9a980fb5fc334183f6a261b8adb8947378d1110
Charles P. Oliver <cpoliver@gmail.com>
2017-05-26 18:40:44 +0200

    Add static list of decisions

--- src/components/pages/Decider.js
+++ src/components/pages/Decider.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
+import decisions from '../../data/decisions.json';
 import pageStyles from './pageStyles';
 import { Header } from '../common';
 
 class Decider extends Component {
+	renderItems() {
+		return decisions.map((item, index) => (
+      <ListItem key={index} title={item.name} />
+		));
+	}
+
   render() {
     return (
       <View style={pageStyles.view}>
         <Header>Decider</Header>
-      </View>
+				<List style={{ flex: 1 }}>
+					{this.renderItems()}
+				</List>
+			</View>
     );
   }
 }
