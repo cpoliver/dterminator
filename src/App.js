eb8a2b37bb08d4a3e83d601cafa6e83b8655ccbe
Charles P. Oliver <cpoliver@gmail.com>
2017-05-27 19:43:11 +0200

    Revert app component to class in order to fix hot reloading

--- src/App.js
+++ src/App.js
@@ -9,21 +9,27 @@ import {
   Decider, DecisionDetail, Home, Profile, Randomizer, Settings
 } from './components/pages';
 
+const store = createStore(reducers);
+
 StatusBar.setBarStyle('light-content', true);
 
-const App = () => (
-  <Provider store={createStore(reducers)}>
-    <Swiper loop={false} showsPagination={false} index={1}>
-      <Randomizer />
-      <Swiper loop={false} showsPagination={false} index={1} horizontal={false}>
-        <Profile />
-        <Home />
-        <Settings />
-      </Swiper>
-      <Decider />
-      <DecisionDetail />
-    </Swiper>
-  </Provider>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Swiper loop={false} showsPagination={false} index={1}>
+          <Randomizer />
+          <Swiper loop={false} showsPagination={false} index={1} horizontal={false}>
+            <Profile />
+            <DecisionDetail />
+            <Home />
+            <Settings />
+          </Swiper>
+          <Decider />
+        </Swiper>
+      </Provider>
+    );
+  }
+}
 
 export default App;
