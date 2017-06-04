8a0c5e0ce246fd8ad8b0d1c9a18d8071a818d85f
Charles P. Oliver <cpoliver@gmail.com>
2017-06-04 23:59:06 +0200

    Add tests for main component

---
+++ src/components/Main.test.js
@@ -0,0 +1,16 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Main from './Main';
+
+const render = () => shallow(<Main />);
+
+describe('the Main component', () => {
+  it('should match the snapshot', () => {
+    const component = render();
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
