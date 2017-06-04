2439a98327869ef5b8eea40e434fedd6c6b822a4
Charles P. Oliver <cpoliver@gmail.com>
2017-06-04 14:28:31 +0200

    Set up unit testing

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('the App component', () => {
+  it('should render', () => {
+    renderer.create(<App />);
+  });
+});
