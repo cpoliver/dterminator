a767eddbc6293169b2fc420f8997a48192baa6db
Charles P. Oliver <cpoliver@gmail.com>
2017-06-04 20:15:31 +0200

    Add coverage for header component

---
+++ src/components/common/Header.test.js
@@ -0,0 +1,20 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import { Header } from './Header';
+
+const render = () => shallow(
+  <Header iconName="account">
+    Header Text
+  </Header>
+);
+
+describe('the Header component', () => {
+  it('should match the snapshot', () => {
+    const component = render();
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
