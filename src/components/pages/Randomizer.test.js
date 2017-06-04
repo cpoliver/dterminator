8fb23a43ed414437ec5bb9e806038694237627c9
Charles P. Oliver <cpoliver@gmail.com>
2017-06-04 23:40:59 +0200

    Add tests for randomizer page component

---
+++ src/components/pages/Randomizer.test.js
@@ -0,0 +1,18 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import { Randomizer } from './Randomizer';
+
+const render = () => shallow(
+  <Randomizer />
+);
+
+describe('the Randomizer component', () => {
+  it('should match the snapshot', () => {
+    const component = render();
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
