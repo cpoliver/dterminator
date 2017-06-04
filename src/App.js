0850094a8ddd840161739fddba8a964f5781a3c4
Charles P. Oliver <cpoliver@gmail.com>
2017-06-04 14:32:06 +0200

    Change redux-logger log level to "info"

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { StatusBar } from 'react-native';
 import { Router } from 'react-native-router-flux';
 import { connect, Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 import { scenes } from './Routing';
 import rootReducer from './reducers';
+
+const logger = createLogger({ level: 'info' });
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 const RouterWithRedux = connect()(Router);
