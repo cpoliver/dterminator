9c07cf72b5dac59ee31caa816dfbe3335cc6d7c1
Charles P. Oliver <cpoliver@gmail.com>
2017-06-04 23:41:28 +0200

    Add tests for profile page component

---
+++ src/components/pages/Profile.test.js
@@ -0,0 +1,18 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import { Profile } from './Profile';
+
+const render = () => shallow(
+  <Profile />
+);
+
+describe('the Profile component', () => {
+  it('should match the snapshot', () => {
+    const component = render();
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
