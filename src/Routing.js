46c39f8aaec0aa7e17f79c84439b142b7c058a18
Charles P. Oliver <cpoliver@gmail.com>
2017-06-05 14:41:15 +0200

    Reapply custom header styles for new router

--- src/Routing.js
+++ src/Routing.js
@@ -2,12 +2,33 @@ import { StackNavigator } from 'react-navigation';
 
 import { Main } from './components/Main';
 import { DecisionDetail } from './components/screens';
+import { colors } from './styles';
+
+const mainRoute = 'main';
 
 const Router = StackNavigator({
-  main: { screen: Main },
-  decisionDetail: { screen: DecisionDetail }
+  [mainRoute]: {
+    screen: Main,
+    navigationOptions: {
+      header: null
+    }
+  },
+  decisionDetail: {
+    screen: DecisionDetail,
+    navigationOptions: {
+      title: 'Decision Detail',
+      headerTintColor: colors.main,
+      headerStyle: {
+        backgroundColor: colors.background
+      },
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: 'bold'
+      }
+    }
+  }
 }, {
-  initialRouteName: 'main'
+  initialRouteName: mainRoute
 });
 
 export default Router;
