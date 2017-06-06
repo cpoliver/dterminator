679a531f68e1da0c347760b0189766289afcd4b1
Charles P. Oliver <cpoliver@gmail.com>
2017-06-06 20:14:24 +0200

    Add form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import savedDecisionsReducer from './savedDecisionsReducer';
 import selectedDecisionReducer from './selectedDecisionReducer';
 
 export default combineReducers({
+  form: formReducer,
   decisions: savedDecisionsReducer,
   selectedDecision: selectedDecisionReducer
 });
